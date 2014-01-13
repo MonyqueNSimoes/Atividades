c03dc0e599a266f1df9e2c8ac819a5b6d1441089
MonyqueNSimoes <mona_agos@hotmail.com>
2014-01-13 18:50:36 -0200

    Create menu.php

---
+++ avaliacao3/menu.php
@@ -0,0 +1,9 @@
+<a href="index.php">Listar de produtos</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">Editar</a>
+
+<br/><br/>
