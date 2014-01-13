8bdee5e981ad756228b4b5bebf21f83fedbba5a7
MonyqueNSimoes <mona_agos@hotmail.com>
2014-01-13 18:47:20 -0200

    Create campos_pessoa.php

---
+++ avaliacao3/campos_pessoa.php
@@ -0,0 +1,23 @@
+<label>Nome: </label>
+<input type="text" name="nome" />
+<br/>
+<input type="checkbox" name="aceito" value="sim" />
+<label>Produto Novo</label>
+<br/>
+<b>Cor do Produto</b><br/>
+Vermelho <input type="radio" name="cor" value="Vermelho" /><br/>
+Azul <input type="radio" name="cor" value="azul"/><br/>
+Verde <input type="radio" name="cor" value="verde"/><br/>
+<br/>
+Prazo de Entrega do produto: <select name="prazo">
+    <option>5 Dias</option>
+    <option>10 Dias</option>
+    <option>15 Dias</option>
+</select>
+</fieldset>
+</select>
+<br/>
+<label>Detalhes</label>
+<br/>
+<textarea name="detalhes"></textarea>
+<br/>
